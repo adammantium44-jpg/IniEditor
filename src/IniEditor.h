d33e98b769648b3259fe018500f59c67223419d0
adammantium44-jpg <adammantium44@gmail.com>
2026-06-16 18:30:58 +0700

    Update IniEditor.h

--- src/IniEditor.h
+++ src/IniEditor.h
@@ -3,6 +3,7 @@
 #include "SKSEMenuFramework.h"
 #include <string>
 #include <map>
+#include <vector>
 
 class IniEditor
 {
@@ -12,10 +13,17 @@ public:
 private:
     static void DrawPage();
 
+    static void ScanIniFiles();
     static void LoadIni();
     static void SaveIni();
 
-    static inline std::string s_iniPath{};
+    // Daftar semua INI di Data/SKSE/Plugins
+    static inline std::vector<std::string> s_iniFiles{};
+
+    // File INI yang sedang dipilih/aktif
+    static inline std::string s_selectedIni{};
+
+    // Data key/value dari file aktif
     static inline std::map<std::string, bool> s_bools{};
     static inline std::map<std::string, float> s_floats{};
     static inline std::map<std::string, std::string> s_strings{};
